1902800148acfaffc109dfeab724084d4c02b0d5
Massimo Ronca <massimo.ronca@leitha.eu>
2024-10-21 14:59:59 +0200

    refactor C3 plugin

--- lua/c3/c3.lua
+++ lua/c3/c3.lua
@@ -6,26 +6,33 @@ vim.filetype.add({
     },
 });
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs();
+-- Add C3 parser configuration
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.c3 = {
     install_info = {
         url = "https://github.com/c3lang/tree-sitter-c3",
         files = { "src/parser.c", "src/scanner.c" },
         branch = "main",
     },
-};
+    sync_install = false, -- Set to true if you want to install synchronously
+    auto_install = true,  -- Automatically install when opening a file
+    filetype = "c3",      -- if filetype does not match the parser name
+}
+
 
 return {
-    recommended = {
-        ft = "c3",
-        root = { "project.json" },
-    },
+    -- Recommended configuration for C3
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
             ensure_installed = { "c3" },
             highlight = { enable = true },
+            sync_install = false, -- Use true if you want synchronous installation
+            auto_install = true,
         },
+        config = function()
+            require("nvim-treesitter.install").prefer_git = true -- Use Git for installation
+        end,
     },
     {
         "neovim/nvim-lspconfig",
@@ -33,8 +40,14 @@ return {
             servers = {
                 c3_lsp = {
                     cmd = { "c3-lsp" },
-                }
-            }
-        }
+                },
+            },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context", -- Optional: For better context display
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-refactor", -- Optional: For code refactoring features
     },
 }
